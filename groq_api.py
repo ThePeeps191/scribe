1a536d6cc684b00eb0a77c4293c500f45404143c
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2026-06-15 21:34:49 -0600

    transcribing audio

--- groq_api.py
+++ groq_api.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
 client = Groq(api_key = GROQ_API_KEY)
 
-def ask_groq(content: str) -> str:
+with open("prompt.txt") as f:
+    PROMPT = f.read()
+
+def ask_groq(content: str):
     chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -20,5 +22,8 @@ def ask_groq(content: str) -> str:
     )
     return chat_completion.choices[0].message.content
 
+def ask_groq_prompt(text: str):
+    return ask_groq(PROMPT + text)
+
 if __name__ == "__main__":
-    print(ask_groq("return the following with corrected grammar: hi im bob and i like to watch youtube"))
+    print(ask_groq("As concise as possible, what is a React component?"))
